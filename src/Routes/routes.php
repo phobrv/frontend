477dba3684c42887125ea898c93008ac64f97e79
phont <phont@arrow-tech.vn>
2023-02-02 11:37:51 +0700

    update

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -6,8 +6,4 @@ Route::middleware('web')->namespace('Phont\Frontend\Controllers')->group(functio
     Route::get('/', 'HomeController@index')->name('home');
     Route::get('/{slug}', 'HomeController@level1')->name('level1');
     Route::get('/{object}/{slug}', 'HomeController@level2')->name('level2');
-    // Route::post('/received', 'HomeController@received')->name('received');
-    // Route::post('/receivedWithReCaptcha', 'HomeController@receivedWithReCaptcha')->name('receivedWithReCaptcha');
-    // Route::post('/order', 'HomeController@order')->name('order');
-    // Route::post('/takeModalRequest', 'HomeController@takeModalRequest')->name('takeModalRequest');
 });
