4f25884d114a91671ea7ebac4476678f56c0c2c8
phont <phont@arrow-tech.vn>
2023-02-07 13:50:42 +0700

    update

--- init_larvel.sh
+++ init_larvel.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 echo Enter project name:
 read projectName
+echo "------------Create project $projectName"
 composer create-project laravel/laravel $projectName
 cd $projectName
+echo "------------Create upload dir for elfinder "
+rm -rf storage/app/.gitignore
+rm -rf storage/app/public/.gitignore
+mkdir storage/app/public/photos 
+mkdir storage/app/public/photos/shares
+echo "------------Frontend package"
 mkdir packages
-cd packages
-mkdir phont
-cd phont
+mkdir packages/phont
+cd packages/phont
 git clone git@github.com:phobrv/frontend.git
 cp frontend/resources/exam/composer.json ../../composer.json
 cd ../../
-echo "$PWD"
+echo "------------Run composer udpate"
 composer update
 php artisan jetstream:install livewire
 php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider"
@@ -19,6 +25,3 @@ php artisan vendor:publish --force --tag=frontend.source
 php artisan vendor:publish --force --tag=brvcore.assets
 npm install
 npm run production
-npm run build
-
-
