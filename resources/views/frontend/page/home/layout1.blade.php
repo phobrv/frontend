b922703c12383443dc6b809d9a135bc63a160447
phont <phont@arrow-tech.vn>
2023-02-27 13:33:01 +0700

    update

--- resources/views/frontend/page/home/layout1.blade.php
+++ resources/views/frontend/page/home/layout1.blade.php
@@ -5,6 +5,12 @@
       @include('phont::frontend.components.boxFormWithReCaptcha')
     <div class="row">
         @for($i=0; $i<100;$i++)
+        {{-- <div class="col-sm-6 col-md-4 mb-2">
+          <img class="responsively-lazy"
+          src="http://127.0.0.1:8000/storage/rs100/shares/panda.jpeg"
+           data-srcset="http://127.0.0.1:8000/storage/rs300/shares/panda.jpeg 300w, http://127.0.0.1:8000/storage/rs400/shares/panda.jpeg 400w,http://127.0.0.1:8000/storage/rs500/shares/panda.jpeg 500w, http://127.0.0.1:8000/storage/rs600/shares/panda.jpeg 600w, http://127.0.0.1:8000/storage/rs800/shares/panda.jpeg 800w, http://127.0.0.1:8000/storage/rs800/shares/panda.jpeg 1000w, http://127.0.0.1:8000/storage/rs800/shares/panda.jpeg 1500w"
+           srcset="http://127.0.0.1:8000/storage/rs100/shares/panda.jpeg" />
+        </div> --}}
         <div class="col-sm-6 col-md-4 mb-2">
             <img class="lazyload"
             src="http://127.0.0.1:8000/storage/rs100/shares/panda.jpeg"
