f7ab1b80123b1eff936ba8965b31947cb0965a0c
phont <phont@arrow-tech.vn>
2023-02-28 09:03:21 +0700

    update

--- init_larvel.sh
+++ init_larvel.sh
@@ -10,16 +10,16 @@ rm -rf storage/app/public/.gitignore
 mkdir storage/app/public/photos 
 mkdir storage/app/public/photos/shares
 echo "------------Install brvcore and frontend"
-composer require phobrv/frontend  dev-main 
-composer require phobrv/brvcore  dev-main 
+#composer require phont/frontend  dev-main 
+#composer require phobrv/brvcore  dev-main 
 php artisan vendor:publish --force --tag=frontend.source
 php artisan vendor:publish --force --tag=brvcore.assets
 php artisan vendor:publish --force --tag=frontend.view 
 echo "------------Run composer udpate"
-composer update
 php artisan jetstream:install livewire
 php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider"
 php artisan elfinder:publish
+composer update
 php artisan vendor:publish --force --tag=frontend.source
 npm install
 npm run production
