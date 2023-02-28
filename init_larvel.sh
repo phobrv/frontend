27f379febe0fe48da8fb41bf3ed1b0608097325a
phont <phont@arrow-tech.vn>
2023-02-28 09:12:25 +0700

    update

--- init_larvel.sh
+++ init_larvel.sh
@@ -2,7 +2,7 @@
 echo Enter project name:
 read projectName
 echo "------------Create project $projectName"
-composer create-project laravel/laravel $projectName
+composer create-project laravel/laravel:^9 $projectName
 cd $projectName
 echo "------------Create upload dir for elfinder "
 rm -rf storage/app/.gitignore
@@ -10,16 +10,15 @@ rm -rf storage/app/public/.gitignore
 mkdir storage/app/public/photos 
 mkdir storage/app/public/photos/shares
 echo "------------Install brvcore and frontend"
-#composer require phont/frontend  dev-main 
-#composer require phobrv/brvcore  dev-main 
-php artisan vendor:publish --force --tag=frontend.source
-php artisan vendor:publish --force --tag=brvcore.assets
-php artisan vendor:publish --force --tag=frontend.view 
+composer require phobrv/brvcore  dev-main 
+composer require phont/frontend  dev-main 
 echo "------------Run composer udpate"
 php artisan jetstream:install livewire
 php artisan vendor:publish --provider="Spatie\Permission\PermissionServiceProvider"
 php artisan elfinder:publish
-composer update
 php artisan vendor:publish --force --tag=frontend.source
+php artisan vendor:publish --force --tag=brvcore.assets
+php artisan vendor:publish --force --tag=frontend.view 
+composer update
 npm install
 npm run production
