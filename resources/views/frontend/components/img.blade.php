7ed387293bb2799adee078df52eb130a1077359f
phont <phont@arrow-tech.vn>
2023-03-07 13:53:24 +0700

    update

--- resources/views/frontend/components/img.blade.php
+++ resources/views/frontend/components/img.blade.php
@@ -4,7 +4,7 @@
     $srcset = (!empty($srcset)) ? $srcset : [2,3,4,5,6,8];
     $data_srcset = "";
     foreach($srcset as $set){
-        $data_srcset = $data_srcset.str_replace('photos', 'rs'.$set.'00' , $source).' '.$set.'00w ,';
+        $data_srcset .= str_replace('photos', 'rs'.$set.'00' , $source).' '.$set.'00w ,';
     }
     $data_srcset = "data-srcset='".substr($data_srcset,0,-1)."'";
 @endphp
